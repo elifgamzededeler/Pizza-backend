21abc12a5af87bf8d15a9804b2f7a63ea21b23c2
elifgamzededeler <gamze.dedeler@gmail.com>
2023-04-11 00:23:17 +0300

    routerlar eklendi

--- api/server.js
+++ api/server.js
@@ -2,4 +2,16 @@ const express = require("express");
 
 const server = express();
 
+const userRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
+const orderRouter = require("./orders/orders-router");
+const malzemelerRouter = require("./malzemeler/malzemeler-router");
+const pizzasRouter = require("./pizzas/pizzas-router");
+
+server.use("./api/auth", authRouter);
+server.use("./api/user", userRouter);
+server.use("api/order", orderRouter);
+server.use("api/malzemeler", malzemelerRouter);
+server.use("api/pizzas", pizzasRouter);
+
 module.exports = server;
